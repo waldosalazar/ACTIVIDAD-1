4b3a6eae2f589032aec8508d8a3469f6a85f0136
waldosalazar <44788983+waldosalazar@users.noreply.github.com>
2019-02-23 10:23:56 -0600

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -2,25 +2,39 @@
 
 Menu::Menu()
 {
-
+    cont = 10;
 }
 
 void Menu::menu()
 {
     string op;
+    fstream mercancia("mercancia.txt",ios::in|ios::app);
+    if(!mercancia.is_open()){
+        cerr<<"No se pudo abrir el archivo con exito"<<endl;
+        exit(1);
+    }
     while (true) {
+        mercancia.clear();
+        mercancia.seekg(0);
         cout<<"1) Alta de producto"<<endl
            <<"2) Consulta"<<endl
-          <<"3) Salir"<<endl
-         <<">> "; getline(cin,op);
+          <<"3) Consulta General"<<endl
+         <<"4) Salir"<<endl
+        <<">> "; getline(cin,op);
         if(op=="1"){
             altaProducto();
         }
         else if(op=="2"){
-            consultaProducto();
+            consultaProducto(mercancia);
         }
         else if(op=="3"){
+            consultaGeneral(mercancia);
+        }
+        else if(op=="4"){
             break;
+        }
+        else{
+            cout<<"Opcion Incorrecta"<<endl;
         }
     }
 }
@@ -33,51 +47,43 @@ void Menu::altaProducto()
     p.setName(palabra);
     cout<<"Descripcion del producto: "; getline(cin,palabra);
     p.setDescription(palabra);
-    p.setId("hola");
-    //generarID();
+    generarID();
     cout<<endl<<endl<<p;
     guardar();
 }
 void Menu::generarID(){
-    char id[15];
-    p.getName().copy(id,2);
-    strcat(id,"-");
+    string id;
+    string complemento;
+    stringstream ss;
+    ss<<cont;
+    id = p.getName().substr(0,2) + "-" + ss.str();
+    cout<<id<<endl;
     p.setId(id);
+    cont++;
 }
-void Menu::consultaProducto()
+void Menu::consultaProducto(fstream& mercancia)
 {
     string llave;
     cout<<"\t[*CONSULTAR PRODUCTO*]"<<endl
        <<"ID: "; getline(cin,llave);
     recuperar(llave);
 }
+
+void Menu::consultaGeneral(fstream& mercancia)
+{
+}
+
 void Menu::guardar()
 {
-    char frase[30];
     ofstream mercancia("mercancia.txt",ios::app);
     if(!mercancia.is_open()){
         cout<<"\t[*ADVERTENCIA*]\nNo se pudo abrir el archivo correctamente"<<endl;
         exit(0);
     }
-    mercancia<<strlen(strcpy(frase,p.getId().c_str()))<<p.getId()<<strlen(strcpy(frase,p.getName().c_str()))<<p.getName()<<strlen(strcpy(frase,p.getDescription().c_str()))<<p.getDescription()<<endl;
+    mercancia<<p.getId().length()<<p.getId()<<p.getName().length()<<p.getName()<<p.getDescription().length()<<p.getDescription()<<endl;
     mercancia.close();
 }
+
 void Menu::recuperar(string llave)
 {
-    string palabra;
-    cout<<"Fuera de while"<<endl;
-    ifstream mercancia("mercancia.txt",ios::in);
-    while(!mercancia.eof()){
-    cout<<"ENTRO"<<endl;
-        getline(mercancia,palabra);
-        cout<<palabra;
-        p.setId(palabra);
-        getline(mercancia,palabra);
-        p.setName(palabra);
-        getline(mercancia,palabra);
-        p.setDescription(palabra);
-        if(p.getId()==llave)
-            cout<<p;
-    }
-
 }
