9b9d676b53adad9487c6f9c8927a100c6e7b0a1e
YoomarXD <yooomarxd@gmail.com>
2019-02-25 20:57:18 -0600

    Update main.cpp

--- solución-2/Actividad 1/main.cpp
+++ solución-2/Actividad 1/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include "Menu.h"
 
+/*
+ *			Estructura de Datos II
+ *	
+ *	Integrantes del equipo:
+ *
+ *	Omar Alberto Pérez Calvario	217758551
+ *	Diego Damián Medina Hernández	213389985
+ *	Alejandro Waldo
+ *
+*/
+
 using namespace std;
 
 int main()
